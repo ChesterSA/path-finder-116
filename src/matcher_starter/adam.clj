52baf1570e7de5744f6b23d69bef688559d4bea5
AdamDuboczky <T7073950@tees.ac.uk>
2019-03-29 14:46:28 +0000

    Started building get-col function

---
+++ src/matcher_starter/adam.clj
@@ -0,0 +1,28 @@
+(def matrix '(3 4 1 2 8 6
+               6 1 8 2 7 4
+               5 9 3 9 9 5
+               8 4 1 3 2 6
+               3 7 2 8 6 4
+               ))
+
+(defn make-matrix [row col matrix]
+  (cond
+    (= 0 row)
+    nil
+    :else
+    (cons (take col matrix)
+          (make-matrix (- row 1) col (drop col matrix)))))
+
+(defn get-at [x y matrix]
+  (nth (nth matrix y) x))
+
+(defn get-first-column ([matrix]
+                        (cond (= () first(matrix))
+                         nil
+  :else
+  (get-first-column matrix (first matrix))
+                         [matrix lis]
+                              (first(lis)))))
+
+
+
